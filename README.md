c7be4db89083d5857a4381d4716490a4bdfe82ba
bawumbila <74751215+bawumbila@users.noreply.github.com>
2021-03-02 17:30:31 -0500

    wip first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Rick and Morty Character Analysis
+
+A sinple wb app that dislays rick and Morty characters and allows you to compare stats.
+
+## ⚙️ Technologies Used
+- HTML5
+- CSS3
+- JS
+- jQuery
+- [Rick and Morty API](https://rickandmortyapi.com/)
+- Github pages for deployment.
+
+## 📸 Screenshots
+
+![Wireframe 1](./imgs/wireframe1.png)
+
+![Wireframe Mobile](./imgs/wireframemobile.png)
+
+## 🏎 💨 Getting Started
+
+[click](#) here to get started with app online. 
+
+
+## 🛠 Future Enhancements
---
+++ js/script.js
@@ -0,0 +1,30 @@
+//IPO Pattern
+
+/*----- constants -----*/
+
+// const BASE_URL = 'put my base url in here';
+
+
+/*----- app's state (variables) -----*/
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+
+alert('JS is Loaded');
+
+
+/*----- functions -----*/
+
+//execute all Initial Actions
+// Infinity() {
+
+    // gather all data
+
+
+// function getData() {
+// $.ajax(BASE_URL)
+// .then(function(data) {
+// console.log(data);
+// }, function(error) {
+//     console.log('error: ', error);
+// });
+// }
