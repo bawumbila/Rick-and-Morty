8e1d5ecef90ccc14f9ab1f8e73dc8b07546e4e0b
bawumbila <74751215+bawumbila@users.noreply.github.com>
2021-03-02 23:29:43 -0500

    wip added AJAX

--- js/script.js
+++ js/script.js
@@ -1,18 +1,33 @@
 //IPO Pattern
 
-/*----- constants -----*/
+ /*----- constants -----*/
 
-// const BASE_URL = 'put my base url in here';
+const BASE_URL = 'https://rickandmortyapi.com/api';
 
 
 /*----- app's state (variables) -----*/
 /*----- cached element references -----*/
 /*----- event listeners -----*/
 
-alert('JS is Loaded');
-
-
 /*----- functions -----*/
+
+// executes all inaitail actions when page loads
+init(); 
+
+function init() {
+// gather all characters when page loads
+getData();
+}
+
+function getData() {
+    $.ajax(BASE_URL)
+    .then(function(data) {
+        console.log('data: ', data);
+    }, function(error) {
+        console.log('error ', error);
+        });
+
+}
 
 //execute all Initial Actions
 // Infinity() {
